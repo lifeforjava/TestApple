79ef39ac4ec2c4f2ff309c6cfa2fee134f3fe427
ankul <lifeforjava786@gmail.com>
2022-12-01 17:24:04 +0530

    committed

---
+++ src/Q36.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Q36 {
+
+
+    public static void main(String[] args) throws IOException {
+
+        //use the scanner is used take the input
+       /* for(int i=0;i<=3;i++){
+            Scanner input=new Scanner(System.in);
+            System.out.println("Entr some Text:: ");
+            String enteredText=input.nextLine();
+            System.out.println(enteredText);
+        }*/
+
+        //rading the file throgh bufffer
+
+        /*File file=new File("myFile.txt");
+        Scanner input=new Scanner(file);
+        while (input.hasNextLine()){
+            String line=input.nextLine();
+            System.out.println(line);
+
+        }
+        input.close();*/
+
+
+
+
+
+
+
+    }
+
+
+}
